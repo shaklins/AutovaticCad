273f8aac070d852c473d9c06ec67f0ea4e7ebc56
Shaklins <Andreiklinsmann@gmail.com>
2024-11-15 19:13:23 +0300

    make file get data acad

---
+++ get_data_acad.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import win32com.client
+
+def get_data_blocks(doc, block_name="_AS_Base_blck"):
+    pass
+
+def generate_data_from_d(dict):
+    """ Получает Б1 и Б2 параметры и координаты
+    Возвращает {координаты:[{param:value},...]} """
+    pass
